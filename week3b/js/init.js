149455d94b0662fe2701edb677173887aab691bf
rebeccaksuen <rebeccaksuen@g.ucla.edu>
2023-06-22 20:31:16 -0700

    lab3

--- week3b/js/init.js
+++ week3b/js/init.js
@@ -1,59 +1,31 @@
+// declare variables
 let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
 
-// declare the map and use mapOptions
+// use the variables
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
 
-// declare the variables
-let mapCenter = [34.0709,-118.444]
-const zoom = 5
-
-// declare the map and use the variables above
-const map = L.map('the_map').setView(mapCenter, zoom);
-console.log("Hello Asia-Am 191A! :)")
-
-// JavaScript const variable declaration
-const map = L.map('the_map').setView([1.352083, 103.819839], 15); 
-
-// Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map); 
+}).addTo(map);
 
-//JavaScript let variable declaration to create a marker
-let marker = L.marker([1.352083, 103.819839]).addTo(map) 
-        .bindPopup('Singapore: Where I studied abroad last summer!')
-        .openPopup();
-
-        function addMarker(lat,lng,message){ 
-            console.log(message) 
-            L.marker([lat,lng]).addTo(map).bindPopup(message)
-            createButtons(lat,lng,title);  
-            return message 
-        }
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    return message
+}
 
 fetch("map.geojson")
     .then(response => {
-        return response.json();
+        return response.json()
     })
     .then(data =>{
         // Basic Leaflet method to add GeoJSON data
-        L.geoJSON(data).addTo(map)
-    });
-
-addMarker(34.414668,-118.556890,'Santa Clarita','My hometown!')
-addMarker(11.584790,122.753212,'Roxas City, Philippines',"My Mom's hometown")
-addMarker(34.068920,-118.445183,'UCLA','School')
-addMarker(37.453220,-122.183220,'Menlo Park',"My Dad's hometown")
-
-function createButtons(lat,lng,title){
-    const newButton = document.createElement("button"); 
-    newButton.id = "button"+title; 
-    newButton.innerHTML = title; 
-    newButton.setAttribute("lat",lat); 
-    newButton.setAttribute("lng",lng); 
-    newButton.addEventListener('click', function(){
-        map.flyTo([lat,lng]); 
+        L.geoJSON(data, {
+                pointToLayer: (feature, latlng) => { 
+                    return L.circleMarker(latlng, {color: feature.properties.color})
+                }
+            }).bindPopup(layer => {
+                return layer.feature.properties.place;
+            }).addTo(map);
     })
-    document.getElementById("contents").appendChild(newButton); 
-}
-
