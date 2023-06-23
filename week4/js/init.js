9a97f64826e81c51f2f7ba7123b977801bb99ed0
rebeccaksuen <rebeccaksuen@g.ucla.edu>
2023-06-22 22:56:43 -0700

    lab4

---
+++ week4/js/init.js
@@ -0,0 +1,36 @@
+// declare variables
+let mapOptions = {'center': [41.4925,-99.9018],'zoom':4}
+
+// use the variables
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    return message
+}
+
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQPRqwlPQiTej0JAecl62AGseYUZYTp2m_kcdNT9BwASN1C4nGXTayPSYGTxUGSaANome7CwLJduCfw/pub?output=csv"
+
+function loadData(url){
+    Papa.parse(url, {
+        header: true,
+        download: true,
+        complete: results => processData(results)
+    })
+}
+
+function processData(results){
+    console.log(results)
+    results.data.forEach(data => {
+        console.log(data)
+        addMarker(data.lat,data.lng,data['Where is your favorite dessert spot located?'],data['What type of dessert does your favorite dessert spot serve?'])
+    })
+}
+
+loadData(dataUrl)
